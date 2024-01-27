5962ade700d6ba850173796255fee55a5c4f9a8f
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-01-27 14:42:33 +0200

    Adding Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a file in /tmp
 
 file { '/tmp/scool':
+  content => 'I Love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I Love Puppet',
 }
