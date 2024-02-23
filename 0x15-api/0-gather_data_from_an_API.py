0c455ed41889d7b5ec4745b25b43ef6e00bdd714
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-23 22:07:15 +0200

    Adding Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,14 +5,14 @@ import requests
 
 if __name__ == 'main':
     user_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                            .format(argv[1])).json()
+                                .format(argv[1])).json()
     tasks = requests.get('https://jsonplaceholder.typicode.com/todos/',
-                        params={'userId': '{}'.format(argv[1])}).json()
+                            params={'userId': '{}'.format(argv[1])}).json()
 
     done_tasks_title = [task['title'] for task in tasks if task['completed']]
     done_tasks = len(done_tasks_title)
 
     print('Employee {} is done with tasks({}/{}):'
-        .format(user_info.get('name'), done_tasks , len(tasks)))
+            .format(user_info.get('name'), done_tasks, len(tasks)))
 
     [print('\t {}'.format(task)) for task in done_tasks_title]
