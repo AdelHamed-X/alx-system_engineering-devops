c0c2a78b866fc467ac6f1e905a03bca951ed03a1
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-23 22:01:27 +0200

    Adding Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" A Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress. """
+
+import requests
+from sys import argv
+
+user_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                         .format(argv[1])).json()
+tasks = requests.get('https://jsonplaceholder.typicode.com/todos/',
+                     params={'userId': '{}'.format(argv[1])}).json()
+
+done_tasks_title = [task['title'] for task in tasks if task['completed']]
+done_tasks = len(done_tasks_title)
+
+print('Employee {} is done with tasks({}/{}):'
+      .format(user_info.get('name'), done_tasks , len(tasks)))
+
+[print('\t {}'.format(task)) for task in done_tasks_title]
