8e3ce424126b12c0789eb8a0d0338ffb059b0c78
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-23 22:05:48 +0200

    Adding Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" A Python script that, using this REST API, for a given employee ID """
-
+""" A Python script that returns todo list info for a given employee ID """
 from sys import argv
 import requests
 
-user_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                         .format(argv[1])).json()
-tasks = requests.get('https://jsonplaceholder.typicode.com/todos/',
-                     params={'userId': '{}'.format(argv[1])}).json()
+if __name__ == 'main':
+    user_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(argv[1])).json()
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos/',
+                        params={'userId': '{}'.format(argv[1])}).json()
 
-done_tasks_title = [task['title'] for task in tasks if task['completed']]
-done_tasks = len(done_tasks_title)
+    done_tasks_title = [task['title'] for task in tasks if task['completed']]
+    done_tasks = len(done_tasks_title)
 
-print('Employee {} is done with tasks({}/{}):'
-      .format(user_info.get('name'), done_tasks , len(tasks)))
+    print('Employee {} is done with tasks({}/{}):'
+        .format(user_info.get('name'), done_tasks , len(tasks)))
 
-[print('\t {}'.format(task)) for task in done_tasks_title]
+    [print('\t {}'.format(task)) for task in done_tasks_title]
