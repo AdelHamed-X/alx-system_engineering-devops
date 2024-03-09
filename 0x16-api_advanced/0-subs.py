9b8f20ae47c75ece58f4ccfc58aecfad3a0ef5eb
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-03-10 00:46:35 +0200

    Adding Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" A script that interacts with reddit api """
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """ gets the subs number of a subreddit """
+    headers = {"User-Agent": "api/1.0 (by /u/AdelHamed-X)"}
+    resp = requests.get(f'https://www.reddit.com/r/{subreddit}/about',
+                        headers=headers)
+    
+    if resp.status_code == 200:
+        try:
+            result = resp.json().get('data')
+            return result.get('subscribers')
+        except:
+            pass
+    else:
+        return 0
