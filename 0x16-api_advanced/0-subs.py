4b9cce9fae40867c8cb06a9b666a73402bd0cdd5
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-03-10 00:47:32 +0200

    Adding Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,12 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "api/1.0 (by /u/AdelHamed-X)"}
     resp = requests.get(f'https://www.reddit.com/r/{subreddit}/about',
                         headers=headers)
-    
+
     if resp.status_code == 200:
         try:
             result = resp.json().get('data')
             return result.get('subscribers')
-        except:
+        except Exception:
             pass
     else:
         return 0
