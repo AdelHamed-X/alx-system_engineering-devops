51eb657260094522187c591ee5a12b9fcabd1ac0
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-03-17 13:42:09 +0200

    Adding Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Handling high amount of requsts
-exec {'replace':
-  provider => shell,
+exec { 'replace' :
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  provider => shell,
   before   => Exec['restart'],
 }
 
-exec {'restart':
-  provider => shell,
+exec { 'restart' :
   command  => 'sudo service nginx restart',
+  provider => shell,
 }
